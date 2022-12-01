da8bcb9049e8d997ba6e5736ab4148841c28a743
paullaster <paullasterokoth98@gmail.com>
2022-12-01 03:59:49 +0300

    db conn

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import mysql from 'mysql2';
 
+dotenv.config ();
+const app = express ();
 
+app.use ( express.json ());
+app.use ( express.urlencoded ( {extended: true} ));
+
+//DB connection:
+const db = mysql.createConnection ( {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+//TEST connection
+db.connect ( (err) => {
+    if (err) {
+        throw err;
+    };
+    console.log ( 'database connection established');
+});
+
+app.post ( '/insert', (req, res) => {
+    const dataToInsert = { ...req.body};
+    db.query ('INSERT INTO SET?', dataToInsert, (err, rows) => {
+        if (err) {
+            res
+            .status (500)
+            .json ( {
+                status: 'error',
+                error: err.message,
+            });
+            return;
+        };
+        res
+        .status (200)
+        .json ( {
+            status: 'success',
+            message: 'Inserted employee record',
+        });
+    });
+} );
+
+app.listen ( process.env.PORT, () => {
+    console.log ( 'listening on port ' + process.env.PORT );
+});
